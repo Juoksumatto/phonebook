9a98fe5d55ac8082693110f5e76fbd7d984f7c9c
onni <holttaonni@gmail.com>
2026-03-03 11:03:09 +0200

    Add files via upload

---
+++ 3.10 tehtävä nettiin/frontend/src/Persons.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Persons = ({ persons, removePerson }) => (
+  <ul>
+    {persons.map(person => (
+      <li key={person.id}>
+        {person.name} {person.number}
+        <button onClick={() => removePerson(person.id)}>
+          delete
+        </button>
+      </li>
+    ))}
+  </ul>
+)
+
+export default Persons
